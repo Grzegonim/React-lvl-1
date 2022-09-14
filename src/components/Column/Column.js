4209d601492fd4283cd872a1b6ecfc5f586931b0
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2022-09-14 16:29:12 +0200

    Add components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,17 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
+import List from '../List/List';
+import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
+  console.log(props.action)
   return (
       <article className={styles.column}>
         <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
+        <ul className={styles.cards}>
+          {props.cards.map(card => <Card key={card.id} title={card.title} />)}          
+        </ul>
+        <CardForm columnId={props.id} action={props.action} />
       </article>
   );
 };
